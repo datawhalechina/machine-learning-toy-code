6cd38570293dd4409f506ff689af09cb2cb64f42
johnjim0816 <johnjim0816@gmail.com>
2021-08-31 21:27:30 +0800

    update

--- SVM/test.py
+++ SVM/test.py
@@ -5,15 +5,14 @@ Author: JiangJi
 Email: johnjim0816@gmail.com
 Date: 2021-08-31 20:31:44
 LastEditor: JiangJi
-LastEditTime: 2021-08-31 20:49:34
+LastEditTime: 2021-08-31 20:59:45
 Discription: 
 Environment: 
 '''
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm
-import seaborn as sns
-sns.set()
+import seaborn as sns;sns.set()
 
 data = np.array([
     [0.1, 0.7],
@@ -30,7 +29,7 @@ data = np.array([
     [0.5, 0.72]
     ])
  
-target = [1] * 6 + [0] * 6
+label = [1] * 6 + [0] * 6
  
 # x_line = np.linspace(0, 1, 100)
 # y_line = 1 - x_line
@@ -39,7 +38,7 @@ target = [1] * 6 + [0] * 6
 # plt.plot(x_line, y_line)
  
  
-# linear_svc = svm.SVC(kernel='linear', C=C).fit(data, target)
+# model_linear = svm.SVC(kernel='linear', C=C).fit(data, label)
  
 x_min, x_max = data[:, 0].min() - 0.2, data[:, 0].max() + 0.2
 y_min, y_max = data[:, 1].min() - 0.2, data[:, 1].max() + 0.2
@@ -53,7 +52,7 @@ titles = ['SVC with linear kernel',
 
  
 for i, gamma in enumerate([1, 5, 15, 35, 45, 55]):
-    rbf_svc = svm.SVC(kernel='rbf', gamma=gamma, C = 0.0001).fit(data, target)
+    rbf_svc = svm.SVC(kernel='rbf', gamma=gamma, C = 0.0001).fit(data, label)
  
     # ravel - flatten
     # c_ - vstack
@@ -71,11 +70,11 @@ for i, gamma in enumerate([1, 5, 15, 35, 45, 55]):
  
     plt.title('RBF SVM with $\gamma=$' + str(gamma))
 
-linear_svc = svm.SVC(kernel='linear', C=0.001).fit(data, target)
+model_linear = svm.SVC(kernel='linear', C=0.001).fit(data, label)
 # ravel - flatten
 # c_ - vstack
 # #把后面两个压扁之后变成了x1和x2，然后进行判断，得到结果在压缩成一个矩形
-Z = linear_svc.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = model_linear.predict(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
 
 plt.subplot(4, 2, 7)
@@ -88,11 +87,11 @@ plt.scatter(data[6:, 0], data[6:, 1], marker='x', color='k', s=100, lw=3)
 
 plt.title('Linear SVM')
 
-linear_svc = svm.SVC(kernel='linear', C=0.001).fit(data, target)
+model_linear = svm.SVC(kernel='linear', C=0.001).fit(data, label)
 # ravel - flatten
 # c_ - vstack
 # #把后面两个压扁之后变成了x1和x2，然后进行判断，得到结果在压缩成一个矩形
-Z = linear_svc.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = model_linear.predict(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
 
 plt.subplot(4, 2, 7)
@@ -105,7 +104,7 @@ plt.scatter(data[6:, 0], data[6:, 1], marker='x', color='k', s=100, lw=3)
 
 plt.title('Linear SVM')
 
-model_poly = svm.SVC(C=0.0001, kernel='poly', degree=10).fit(data, target) # 多项式核
+model_poly = svm.SVC(C=0.0001, kernel='poly', degree=10).fit(data, label) # 多项式核
 
 Z = model_poly.predict(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
