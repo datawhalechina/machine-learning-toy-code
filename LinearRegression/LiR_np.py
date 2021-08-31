fabd8deca9687a0ffaabc6a64ffc7bf8c436bcb2
johnjim0816 <johnjim0816@gmail.com>
2021-08-31 20:29:13 +0800

    update

---
+++ LinearRegression/LiR_np.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: JiangJi
+Email: johnjim0816@gmail.com
+Date: 2021-08-27 11:50:51
+LastEditor: JiangJi
+LastEditTime: 2021-08-31 20:21:14
+Discription: 
+Environment: 
+'''
+    
